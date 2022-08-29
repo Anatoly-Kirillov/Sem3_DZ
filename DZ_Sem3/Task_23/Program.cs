ff4b3a2269f1f353e4a04e955729983dc3ad1c2b
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-29 15:30:49 +0300

    Add Task_23

---
+++ DZ_Sem3/Task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+int N = 1;
+
+while (N <= numberN)
+{
+    Console.WriteLine($"{Math.Pow(N,3)}");
+    N = N + 1;
+}
